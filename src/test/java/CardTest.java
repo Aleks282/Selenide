8a54a77c6b57c0c2ec816f93ef655e5c343db38b
Aleksander <alex-vodon@mail.ru>
2023-05-04 16:42:53 +0300

    first commit

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,35 +1,34 @@
-package ru.netology.selenide;
+package ru.netology.web;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
-
-    private String generateDate(int addDays) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.mm.yyyy"));
-    }
-
     @Test
-    public void shouldBeSuccessfulForm() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Москва");
-        String currentDate = generateDate(3);
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id=date] input").sendKeys(currentDate);
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+79856251580");
-        $("[data-test-id=agreement]").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+    public void shouldSentForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id = city] input").setValue("Москва");
+
+        Calendar date = Calendar.getInstance();
+        date.add(Calendar.DATE, 6);
+        String formattedDate = new SimpleDateFormat("dd.MM.yyyy").format(date.getTime());
+        $("[data-test-id = date] input").doubleClick().sendKeys(" ");
+        $("[data-test-id = date] input").setValue(formattedDate);
+
+        $("[data-test-id = name] input").setValue("Иванов Иван");
+        $("[data-test-id = phone] input").setValue("+79508215070");
+        $("[data-test-id = agreement]").click();
+        $$("form button").last().click();
+
+        String expected = "Встреча успешно забронирована на " + formattedDate;
+        $("[data-test-id = notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text(expected));
+
     }
 }
